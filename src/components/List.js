fa7859f80f181a9a42b93b8e6dcfc88d8425e01b
victor canedo <victorcanedo1@hotmail.com>
2023-04-13 16:28:17 -0300

    created fragments

---
+++ src/components/List.js
@@ -0,0 +1,19 @@
+import Item from './Item'
+
+const List = () => {
+  return (
+    <>
+
+      <h1>My List</h1>
+
+      <ul>
+        <Item brand="Ford" />
+        <Item brand="Ferrari" />
+        <Item brand="Fiat" />
+      </ul>
+
+    </>
+  )
+}
+
+export default List
